ab1730a8348226915aab996b01fe7b836cfd396c
matiasandina <matiasandina@gmail.com>
2021-08-13 12:38:48 -0400

    adding first iteration of working functions

---
+++ R/fed_col_types.R
@@ -0,0 +1,45 @@
+# This function
+#' @param version character version of FED_library used to collect data
+fed_col_types <- function(lib_version=NULL){
+
+  # this list is hardcoded
+  col_classes <-
+    list(
+      list(version = "1.8.1",
+           columns = readr::cols(
+             `MM:DD:YYYY hh:mm:ss` = readr::col_character(),
+             Library_Version = readr::col_character(),
+             Session_type = readr::col_character(),
+             Device_Number = readr::col_double(),
+             Battery_Voltage = readr::col_double(),
+             Motor_Turns = readr::col_double(),
+             FR = readr::col_double(),
+             Event = readr::col_character(),
+             Active_Poke = readr::col_character(),
+             Left_Poke_Count = readr::col_double(),
+             Right_Poke_Count = readr::col_double(),
+             Pellet_Count = readr::col_double(),
+             Block_Pellet_Count = readr::col_double(),
+             Retrieval_Time = readr::col_double(),
+             InterPellet_Retrieval_Time = readr::col_double(),
+             Poke_Time = readr::col_double())
+      )
+    )
+
+  if(is.null(lib_version) == FALSE && is.character(lib_version) == FALSE){
+    stop(glue::glue("Version must be NULL or character, it was {class(lib_version)}"))
+  }
+  # gather available versions in hardcoded list
+  versions <- unlist(purrr::map(col_classes, "version"))
+  if(is.null(lib_version)) {
+      # default to last one if we can't find the version
+      return(dplyr::last(col_classes)$columns)
+    } else if(lib_version %in% versions == FALSE){
+      return(dplyr::last(col_classes)$columns)
+    } else {
+    version_index <- stringr::str_which(string = versions, pattern = lib_version)
+    return(col_classes[[version_index]]$columns)
+  }
+
+}
+
---
+++ R/read_fed.R
@@ -0,0 +1,23 @@
+# This function reads CSV files as saved by FED3 Library
+# It will do basic parsing of column classes and return a data.frame
+#' @param filename file path of csv file produced by FED3
+#' @details This function will read the raw data with column specifications (see `fed_col_types()`). It will append the `datetime` by formatting the FED's clock datetime as `yyyy-mm-dd HH:MM:SS`, it will also extract `year`, `month`, and `day` from that date. It will parse the session from the `filename` and append it to the resulting `data.frame`.
+#' @export
+read_fed <- function(filename, lib_version = NULL){
+
+  col_types <- fed_col_types(lib_version = lib_version)
+  X <- readr::read_csv(filename, col_types = col_types) %>%
+    # add datetime, parse session from filename
+    dplyr::mutate(
+      datetime = lubridate::parse_date_time(`MM:DD:YYYY hh:mm:ss`, "mdy HMS"),
+      session = stringr::str_extract(tolower(filename), pattern="_[0-9]+\\.csv$"),
+      session = stringr::str_extract(session, pattern="[0-9]+"))
+
+
+  # Add year, day month columns
+  X <- X %>%
+    dplyr::mutate(day = lubridate::day(datetime),
+                  month = lubridate::month(datetime),
+                  year = lubridate::year(datetime))
+  return(X)
+}
