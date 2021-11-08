bd2cc88d27fed1a551889d6bcf1225b9701c50c5
Matias Andina <matiasandina@gmail.com>
2021-11-08 10:59:52 -0500

    adding new column types for version 1.9+

--- R/fed_col_types.R
+++ R/fed_col_types.R
@@ -24,6 +24,26 @@ fed_col_types <- function(lib_version=NULL){
              Retrieval_Time = readr::col_double(),
              InterPelletInterval = readr::col_double(),
              Poke_Time = readr::col_double())
+      ),
+      list(version = "1.9.2",
+           columns = readr::cols(
+             `MM:DD:YYYY hh:mm:ss` = readr::col_character(),
+             Library_Version = readr::col_character(),
+             Session_type = readr::col_character(),
+             Device_Number = readr::col_double(),
+             Battery_Voltage = readr::col_double(),
+             Motor_Turns = readr::col_double(),
+             FR = readr::col_double(),
+             Event = readr::col_character(),
+             Active_Poke = readr::col_character(),
+             Left_Poke_Count = readr::col_double(),
+             Right_Poke_Count = readr::col_double(),
+             Pellet_Count = readr::col_double(),
+             Block_Pellet_Count = readr::col_double(),
+             Retrieval_Time = readr::col_double(),
+             InterPelletInterval = readr::col_double(),
+             Poke_Time = readr::col_double(),
+             InterPellet_Retrieval_Time = readr::col_double())
       )
     )
 
@@ -34,8 +54,10 @@ fed_col_types <- function(lib_version=NULL){
   versions <- unlist(purrr::map(col_classes, "version"))
   if(is.null(lib_version)) {
       # default to last one if we can't find the version
+      message("FED lib_version not provided, using {dplyr::last(versions)}")
       return(dplyr::last(col_classes)$columns)
     } else if(lib_version %in% versions == FALSE){
+      message("FED lib_version {lib_version} not found in verions, using {dplyr::last(versions)}")
       return(dplyr::last(col_classes)$columns)
     } else {
     version_index <- stringr::str_which(string = versions, pattern = lib_version)
