40600c961aeac2690e2fe3f4bb1396b3a5c59a2f
Matias Andina <matiasandina@gmail.com>
2023-12-11 16:47:49 -0500

    updating set_alignment to partially address #6

--- R/alignment.R
+++ R/alignment.R
@@ -20,33 +20,49 @@
 #'
 set_alignment <- function(df, datetime_col, alignment){
 
-  options <- c('time', 'elapsed')
+  options <- c('midnight', 'time', 'elapsed')
 
   if(!(alignment %in% options)){
     stop(paste("`alignment` must be one of", toString(options), "not", alignment))
   }
 
   min_datetime <- df %>% dplyr::pull({{datetime_col}}) %>% min()
-  min_date <- lubridate::floor_date(min_datetime, unit = "day")
+  # This will keep HH:MM:SS as 00:00:00
+  min_date_midnight <- lubridate::floor_date(min_datetime, unit = "day")
+  # This will not contain HH:MM:SS info
+  min_date <- as.Date(min_datetime)
 
   if(!dplyr::is_grouped_df(df)){
     warning("Input dataframe must be grouped.\nYou should use `df %>% group_by() %>% set_alignment(...)`")
   }
 
-  if(alignment == 'time'){
+  if(alignment == 'midnight'){
     df <- df %>%
       # Calculate the elapsed time for each timestamp relative to the first timestamp in its group
       dplyr::mutate(elapsed = difftime({{datetime_col}},
                                        min({{datetime_col}}),
                                        units = "secs")) %>%
       # Shift each timestamp by adding the elapsed time to the min_date
+      # The min_datetime will become midnight in the new dataset
+      # This will distort the data when the different groups have different starting HH:MM:SS
+      # or the starting HH:MM:SS of the different events varies because animals did not interact with fed3
       dplyr::mutate(dplyr::across(
         .cols = dplyr::where(lubridate::is.POSIXct),
-        .fns = ~ min_date + as.difftime(elapsed, units = "secs"),
+        .fns = ~ min_date_midnight + as.difftime(elapsed, units = "secs"),
         .names = "aligned_{.col}")) %>%
       # Clean up the intermediate elapsed column
       dplyr::select(-elapsed)
-  } else if(alignment == 'elapsed'){
+  } else if (alignment == "time"){
+    df <- df %>%
+      dplyr::mutate(date_diff = difftime(as.Date({{datetime_col}}),
+                                         min_date, units = "days")) %>%
+      dplyr::mutate(dplyr::across(
+      .cols = dplyr::where(lubridate::is.POSIXct),
+      .fns = ~ {{datetime_col}} - date_diff,
+      .names = "aligned_{.col}"
+    ))  %>%
+      dplyr::select(-date_diff)
+  }  else if(alignment == 'elapsed'){
     df <- dplyr::mutate(df, dplyr::across(
       .cols = dplyr::where(lubridate::is.POSIXct),
       .fns = ~ difftime(.x, min(.x), units = "hours"),
