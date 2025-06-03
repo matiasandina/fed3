08cf0b7af69fa1470d3196328a7844b2b486ad7d
matiasandina <matiasandina@gmail.com>
2025-06-03 16:24:38 -0400

    updating make_lights to clip shading to data

--- R/make_lights.R
+++ R/make_lights.R
@@ -10,22 +10,27 @@ make_lights <- function(params, df) {
   # If no event is registered for a long time, the range might be wrong
   experiment_range <- range(df$datetime)
 
-  params$fed_dates <- seq(lubridate::date(experiment_range[1]),
-                          lubridate::date(experiment_range[2]),
-                          "1 day")
+  params$fed_dates <- seq(
+    lubridate::date(experiment_range[1]),
+    lubridate::date(experiment_range[2]),
+    "1 day"
+  )
 
   #make the dates
   # this vector will have on, off, on, off..
   # the first value might be before the experiment starts, the last value after experiment ends
   light_changes <-
-    lapply(params$fed_dates, function(tt)
-      paste(tt, params$lights)) %>%
+    lapply(params$fed_dates, function(tt) paste(tt, params$lights)) %>%
     unlist()
   #make it datetime
   light_changes <- lubridate::as_datetime(light_changes)
   # subset
   light_changes <-
-    light_changes[dplyr::between(light_changes, experiment_range[1], experiment_range[2])]
+    light_changes[dplyr::between(
+      light_changes,
+      experiment_range[1],
+      experiment_range[2]
+    )]
 
   light_diff <- diff(hms::as_hms(params$lights))
 
@@ -61,15 +66,15 @@ make_lights <- function(params, df) {
       last_lights_on <- dplyr::last(light_changes) + light_diff
       light_changes <-
         append(light_changes, values = last_lights_on)
-      full_exp_shade <-ggplot2::annotate(
-          "rect",
-          xmin = light_changes[seq_along(light_changes) %% 2 > 0],
-          xmax = light_changes[seq_along(light_changes) %% 2 == 0],
-          ymin = 0,
-          ymax = Inf,
-          fill = "gray80",
-          alpha = 0.5
-        )
+      full_exp_shade <- ggplot2::annotate(
+        "rect",
+        xmin = light_changes[seq_along(light_changes) %% 2 > 0],
+        xmax = light_changes[seq_along(light_changes) %% 2 == 0],
+        ymin = 0,
+        ymax = Inf,
+        fill = "gray80",
+        alpha = 0.5
+      )
     }
   } else {
     full_exp_shade <-
@@ -85,7 +90,6 @@ make_lights <- function(params, df) {
       )
   }
 
-
   return(full_exp_shade)
 }
 
@@ -96,12 +100,18 @@ make_lights <- function(params, df) {
 #'  `r lifecycle::badge("experimental")`
 #'  A custom ggplot2 extension to shade areas in a plot according to provided light hours.
 #'  It shades from lights off to lights on. Usage through [geom_lights()] or [stat_ligths()]
+#'  By default, shading is clipped to the actual range of the data to avoid misleading over-extension.
+#'  Setting `clip_to_data = FALSE` causes shading to fill the panel (via -Inf/Inf), which can be useful
+#'  but may behave unexpectedly if you manually set axis limits.
 #'
 #' @param lights_on_hour An integer representing the hour of the day (0-23) when the lights turn on.
 #' @param lights_off_hour An integer representing the hour of the day (0-23) when the lights turn off.
 #' @param fill A string representing the color of the shading.
 #' @param alpha A numeric representing the transparency of the shading, between 0 (transparent) and 1 (opaque).
-#'
+#' @param clip_to_data Logical. If TRUE (default), shading is clipped to the range of the data.
+#'   If FALSE, shading extends beyond the data using -Inf/Inf. This can create full-panel shading,
+#'   but may cause unexpected visuals if combined with custom `scale_x_datetime()` limits.
+
 #' @return A ggplot2 layer to be added to a ggplot object.
 #' @export
 #'
@@ -112,67 +122,138 @@ make_lights <- function(params, df) {
 #'   geom_line()
 #'
 #' }
-StatLights <- ggplot2::ggproto("StatLights", ggplot2::Stat,
-                               compute_panel = function(self, data, scales, lights_on_hour = 7, lights_off_hour = 19, time_zone = Sys.timezone()) {
-                                 # Create a copy of data$x converted to POSIXct
-                                 datetime_x <- as.POSIXct(data$x, origin = "1970-01-01", tz = time_zone)
+StatLights <- ggplot2::ggproto(
+  "StatLights",
+  ggplot2::Stat,
+  compute_panel = function(
+    self,
+    data,
+    scales,
+    lights_on_hour = 7,
+    lights_off_hour = 19,
+    time_zone = Sys.timezone(),
+    clip_to_data = TRUE
+  ) {
+    # Create a copy of data$x converted to POSIXct
+    datetime_x <- as.POSIXct(data$x, origin = "1970-01-01", tz = time_zone)
 
-                                 experiment_range <- range(datetime_x)
+    experiment_range <- range(datetime_x)
 
-                                 # Expand the date range to include one day before and after
-                                 extended_dates <- seq(lubridate::date(experiment_range[1]) - lubridate::days(1),
-                                                       lubridate::date(experiment_range[2]) + lubridate::days(1), "1 day")
+    # Expand the date range to include one day before and after
+    extended_dates <- seq(
+      lubridate::date(experiment_range[1]) - lubridate::days(1),
+      lubridate::date(experiment_range[2]) + lubridate::days(1),
+      "1 day"
+    )
 
-                                 # Generate the complete sequence of light changes
-                                 light_on_times <- as.POSIXct(glue::glue("{extended_dates} {lights_on_hour}:00:00"), tz = time_zone)
-                                 light_off_times <- as.POSIXct(glue::glue("{extended_dates} {lights_off_hour}:00:00"), tz = time_zone)
+    # Generate the complete sequence of light changes
+    light_on_times <- as.POSIXct(
+      glue::glue("{extended_dates} {lights_on_hour}:00:00"),
+      tz = time_zone
+    )
+    light_off_times <- as.POSIXct(
+      glue::glue("{extended_dates} {lights_off_hour}:00:00"),
+      tz = time_zone
+    )
 
-                                 # If the off hour is before the on hour, shift the off times to the next day
-                                 if (lights_off_hour < lights_on_hour) {
-                                   light_off_times <- light_off_times + lubridate::days(1)
-                                 }
+    # If the off hour is before the on hour, shift the off times to the next day
+    if (lights_off_hour < lights_on_hour) {
+      light_off_times <- light_off_times + lubridate::days(1)
+    }
 
-                                 # Combine and sort the light changes
-                                 light_changes <- sort(c(light_on_times, light_off_times))
+    # Combine and sort the light changes
+    light_changes <- sort(c(light_on_times, light_off_times))
 
-                                 # Filter to match the actual experiment range, including the nearest on/off times
-                                 light_changes <- light_changes[dplyr::between(light_changes,
-                                                                               experiment_range[1] - lubridate::days(1),
-                                                                               experiment_range[2] + lubridate::days(1))]
+    # Filter to match the actual experiment range, including the nearest on/off times
+    light_changes <- light_changes[dplyr::between(
+      light_changes,
+      experiment_range[1] - lubridate::days(1),
+      experiment_range[2] + lubridate::days(1)
+    )]
 
-                                 # Make sure the sequence starts with an off time and ends with an on time
-                                 if (!length(light_changes) || (lubridate::hour(light_changes[1]) == lights_on_hour && lights_on_hour != lights_off_hour)) {
-                                   light_changes <- light_changes[-1]
-                                 }
-                                 if (!length(light_changes) || (lubridate::hour(tail(light_changes, 1)) == lights_off_hour && lights_on_hour != lights_off_hour)) {
-                                   light_changes <- light_changes[-length(light_changes)]
-                                 }
+    # Make sure the sequence starts with an off time and ends with an on time
+    if (
+      !length(light_changes) ||
+        (lubridate::hour(light_changes[1]) == lights_on_hour &&
+          lights_on_hour != lights_off_hour)
+    ) {
+      light_changes <- light_changes[-1]
+    }
+    if (
+      !length(light_changes) ||
+        (lubridate::hour(tail(light_changes, 1)) == lights_off_hour &&
+          lights_on_hour != lights_off_hour)
+    ) {
+      light_changes <- light_changes[-length(light_changes)]
+    }
 
-                                 # Extract the shading starts and ends
-                                 shading_starts <- light_changes[seq(1, length(light_changes), by = 2)]
-                                 shading_ends <- light_changes[seq(2, length(light_changes), by = 2)]
+    # Extract the shading starts and ends
+    shading_starts <- light_changes[seq(1, length(light_changes), by = 2)]
+    shading_ends <- light_changes[seq(2, length(light_changes), by = 2)]
 
-                                 # Create the data frame
-                                 df <- data.frame(
-                                   xmin = shading_starts,
-                                   xmax = shading_ends,
-                                   ymin = -Inf,
-                                   ymax = Inf
-                                 )
+    # Create the data frame
+    df <- data.frame(
+      xmin = shading_starts,
+      xmax = shading_ends,
+      ymin = -Inf,
+      ymax = Inf
+    )
 
-                                 # Filter to include shading that starts within the actual experiment range
-                                 # or ends within the range
-                                 df <- df %>%
-                                   dplyr::filter((dplyr::between(xmin, experiment_range[1], experiment_range[2])) |
-                                                   (dplyr::between(xmax, experiment_range[1], experiment_range[2])))
+    if (clip_to_data) {
+      df <- df %>%
+        dplyr::filter(
+          xmin <= experiment_range[2] & xmax >= experiment_range[1]
+        ) %>%
+        dplyr::mutate(
+          xmin = pmax(xmin, experiment_range[1]),
+          xmax = pmin(xmax, experiment_range[2])
+        )
+    } else {
+      df <- df %>%
+        dplyr::filter(
+          xmin <= experiment_range[2] & xmax >= experiment_range[1]
+        ) %>%
+        dplyr::mutate(
+          xmin = ifelse(
+            xmin < experiment_range[1],
+            {
+              warning(
+                "Shading xmin extended to -Inf; check scale_x_datetime() if plot looks off."
+              )
+              -Inf
+            },
+            xmin
+          ),
+          xmax = ifelse(
+            xmax > experiment_range[2],
+            {
+              warning(
+                "Shading xmax extended to Inf; check scale_x_datetime() if plot looks off."
+              )
+              Inf
+            },
+            xmax
+          )
+        )
+    }
 
-                                 return(df)
-                               },
-                               required_aes = c("x")
+    return(df)
+  },
+  required_aes = c("x")
 )
 
 #' @export
-geom_lights <- function(mapping = NULL, data = NULL, position = "identity", ..., fill = "gray60", alpha = 0.5, lights_on_hour = 7, lights_off_hour = 19, time_zone = Sys.timezone()) {
+geom_lights <- function(
+  mapping = NULL,
+  data = NULL,
+  position = "identity",
+  ...,
+  fill = "gray60",
+  alpha = 0.5,
+  lights_on_hour = 7,
+  lights_off_hour = 19,
+  time_zone = Sys.timezone()
+) {
   ggplot2::layer(
     stat = StatLights,
     data = data,
@@ -192,7 +273,17 @@ geom_lights <- function(mapping = NULL, data = NULL, position = "identity", ...,
   )
 }
 
-stat_lights <- function(mapping = NULL, data = NULL, geom = "lights", position = "identity", ..., fill = "gray60", alpha = 0.5, lights_on_hour = 7, lights_off_hour = 19) {
+stat_lights <- function(
+  mapping = NULL,
+  data = NULL,
+  geom = "lights",
+  position = "identity",
+  ...,
+  fill = "gray60",
+  alpha = 0.5,
+  lights_on_hour = 7,
+  lights_off_hour = 19
+) {
   ggplot2::layer(
     stat = StatLights,
     data = data,
